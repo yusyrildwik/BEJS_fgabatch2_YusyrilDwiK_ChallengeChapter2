9c1adaaad727575d2738295d6d83a9a1bd37e362
yusyrildwik <dwiyusril@gmail.com>
2024-06-28 23:25:14 +0700

    first code

---
+++ aaa/banking_system.js
@@ -0,0 +1,21 @@
+
+let saldo = 0;
+
+class BankAccount {
+    constructor(account, amount){
+        this.account = account;
+        this.amount = amount;
+    }
+
+    deposit(){
+
+    }
+
+    withdraw(){
+        
+    }
+
+    updateSaldoDisplay(){
+        
+    }
+}
