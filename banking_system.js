bdf8ae1c3c6ccac33711966428aa99081f541416
yusyrildwik <dwiyusril@gmail.com>
2024-06-28 23:34:50 +0700

    edited

--- banking_system.js
+++ banking_system.js
@@ -1,21 +1,41 @@
 
-let saldo = 0;
 
 class BankAccount {
-    constructor(account, amount){
+    constructor(account, balance){
         this.account = account;
-        this.amount = amount;
+        this.balance = balance;
     }
 
-    deposit(){
-
+    deposit(jumlah){
+        return new Promise((resolve, reject) =>{
+            setTimeout(() => {
+                if (!isNaN(jumlah) && jumlah > 0 ){
+                    this.balance += jumlah;
+                    resolve (`Saldo berhasil ditambahkan. Saldo baru: ${this.balance}.`)
+                } else{
+                    reject (`Input tidak valid. Saldo tidak berubah. Silakan coba lagi.`);
+                
+                }
+            }, 1000);
+        })
     }
 
-    withdraw(){
-        
+    withdraw(jumlah){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (!isNaN(jumlah) && jumlah > 0 && jumlah <= this.balance){
+                    this.balance -= jumlah;
+                    resolve(`Saldo berhasil dikurangi. Saldo baru: ${this.balance}.`)
+                } else if (jumlah > this.balance) {
+                    reject(`Saldo tidak mencukupi untuk melakukang pengurangan. `)
+                } else {
+                    reject (`Input tidak valid. Saldo tidak berubah. Silakan coba lagi.`)
+                }
+
+            }, 1000);
+        })
     }
 
-    updateSaldoDisplay(){
-        
-    }
+    
+
 }
